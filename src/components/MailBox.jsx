d4c8b9c28f83840c4c9628fb55612ef9827b4d18
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-03 23:16:14 +0200

    add listener

--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -1,10 +1,19 @@
 const MailBox = () => {
+  const handleClick = () => {
+    console.log("Email has been successfully sent");
+  };
+
   return (
     <div>
       <h2>MailBox</h2>
-      <li>Mail 1</li>
-      <li>Mail 2</li>
-      <li>Mail 3</li>
+      <ul>
+        <li>Mail 1</li>
+        <li>Mail 2</li>
+        <li>Mail 3</li>
+      </ul>
+      <button onClick={handleClick} type="button">
+        Send mail
+      </button>
     </div>
   );
 };
