c8c4308995032e0ddad11c3b1658ce098caca19b
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-11 22:33:12 +0200

    finish with control form

--- src/components/MailBoxForm/MailBoxForm2.jsx
+++ src/components/MailBoxForm/MailBoxForm2.jsx
@@ -5,35 +5,52 @@ import { useState } from "react";
 //!КОНТРОЛЬОВАНА ФОРМА
 const MailBoxForm2 = ({ addNewData }) => {
   //* створюємо стейст для відображення інтерактивності у формі (контрольована форма)
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
+  //   const [userName, setUserName] = useState("");
+  //   const [email, setEmail] = useState("");
+
+  //? ЗБЕРІГАЄМО СТЕЙТ В ОБ'ЄКТІ (ВИКОРИСТ КОЛИ БАГАТО ПОЛІВ)
+  const [values, setValues] = useState({
+    userName: "",
+    email: "",
+  });
 
   //*привязуємо дані які ввели в інтуп щоб відображалися у стейт
   const handleChange = (evt) => {
-    if (evt.target.name === "userName") {
-      setUserName(evt.target.value);
-    } else if (evt.target.name === "email") {
-      setEmail(evt.target.value);
-    }
+    //     if (evt.target.name === "userName") {
+    //   setUserName(evt.target.value);
+    //     } else if (evt.target.name === "email") {
+    //      setEmail(evt.target.value);
+    //     }
+    //? пишемо іншу логіку для стейта в об'єкті
+    const key = evt.target.name;
+    const value = evt.target.value;
+
+    setValues({
+      ...values, // * копіювання попереднього стейту
+      [key]: value,
+    });
   };
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
 
-    //*витягуємо дані з форми
-    const elements = evt.currentTarget.elements;
-
-    const userName = elements.userName.value;
-    const email = elements.email.value;
-
+    //* тепер отримуємо дані зі стейту
     const formData = {
-      userName,
-      email,
+      userName: values.userName,
+      email: values.email,
     };
 
     //* викликаємо функцію додавання поля
     addNewData(formData);
 
-    evt.currentTarget.reset();
+    //* тепер очищаємо форму через стейти (бо через reset працює лише для DOM)
+    // setValues.email("");
+    // setValues.userName("");
+
+    //? для стейту в об'єкту використовуємо пустий об'єкт
+    setValues({
+      userName: "",
+      email: "",
+    });
   };
   return (
     <form onSubmit={handleFormSubmit} className={css.form}>
@@ -46,7 +63,7 @@ const MailBoxForm2 = ({ addNewData }) => {
           placeholder="Alex Igorov"
           type="text"
           name="userName"
-          value={userName} //? ЗВЯЗУЄМО ІНПУТ І СТЕЙТ (value)
+          value={values.userName} //? ЗВЯЗУЄМО ІНПУТ І СТЕЙТ (value)
           required
         />
       </label>
@@ -58,10 +75,14 @@ const MailBoxForm2 = ({ addNewData }) => {
           placeholder="alex.igorov@gmail.com"
           type="email"
           name="email"
-          value={email}
+          value={values.email}
           required
         />
       </label>
+      {/* // * ДОДАЄМО ІНТЕРАКТИВНІСТЬ (ПРОМОКОД)  */}
+      {values.userName === "slava2024" && (
+        <p> Congratulations! You`re winner! Your promo: winner_4587</p>
+      )}
       <button
         className={css.btn}
         type="submit"
